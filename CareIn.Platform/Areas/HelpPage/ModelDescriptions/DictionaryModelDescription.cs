67cbd61583d64e7566d6d2942f0bb0cb08fa0506
Carlos Brito <carlos.brito@msitbrasil.com>
2018-06-06 10:34:32 -0300

    api

---
+++ CareIn.Platform/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CareIn.Platform.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CareIn.Platform/Controllers/NurseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CareIn.Platform.Controllers
+{
+    public class NurseController : ApiController
+    {
+        [HttpGet]
+        [Route("api/GrupoByID")]
+        public int GrupoByID(int GruCod)
+        {
+            return 1;
+        }
+    }
+}
